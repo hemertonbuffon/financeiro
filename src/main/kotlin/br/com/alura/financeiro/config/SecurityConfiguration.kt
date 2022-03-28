5bcf2b1a5d9e4b4db5794418e96c281c737ed3ff
Hemerton <hemertonbuffon@gmail.com>
2022-03-28 11:46:47 -0300

    Adicionada autenticação por JWT

--- src/main/kotlin/br/com/alura/financeiro/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/financeiro/config/SecurityConfiguration.kt
@@ -1,5 +1,7 @@
 package br.com.alura.financeiro.config
 
+import br.com.alura.financeiro.security.JWTAutheticationFilter
+import br.com.alura.financeiro.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -11,25 +13,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
-    private val userDetailsService: UserDetailsService
+    private val userDetailsService: UserDetailsService,
+    private val jwtUtil: JWTUtil
 ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         http?.
+        csrf()?.disable()?.
         authorizeRequests()?.
+        antMatchers(HttpMethod.POST,"/login")?.permitAll()?.
         anyRequest()?.
         authenticated()?.
-        and()?.
-        sessionManagement()?.
-        sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
-        and()?.
-        formLogin()?.
-        disable()?.
-        httpBasic()
+        and()
+        http?.addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.addFilterBefore(JWTAutheticationFilter(jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.sessionManagement()?.
+        sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
     override fun configure(web: WebSecurity?) {
---
+++ src/main/kotlin/br/com/alura/financeiro/model/Credentials.kt
@@ -0,0 +1,8 @@
+package br.com.alura.financeiro.model
+
+data class Credentials(
+    val username: String = "",
+    val password: String = ""
+) {
+
+}
